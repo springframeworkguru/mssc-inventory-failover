38f3a3c3b22ce86e13fcef3efd5e30ba2c316c90
John Thompson <john@springframework.guru>
2020-03-14 15:54:08 -0400

    updating to return list of invDTO objects

--- src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
+++ src/main/java/guru/sfg/brewery/inventoryfailover/web/InventoryHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -18,12 +20,13 @@ public class InventoryHandler {
     public Mono<ServerResponse> listInventory(ServerRequest request){
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Mono.just(BeerInventoryDto.builder()
-                    .id(UUID.randomUUID())
-                    .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
-                    .quantityOnHand(999)
-                    .createdDate(OffsetDateTime.now())
-                    .lastModifiedDate(OffsetDateTime.now())
-                        .build()), BeerInventoryDto.class);
+                .body(Mono.just(Arrays.asList(
+                        BeerInventoryDto.builder()
+                            .id(UUID.randomUUID())
+                            .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+                            .quantityOnHand(999)
+                            .createdDate(OffsetDateTime.now())
+                            .lastModifiedDate(OffsetDateTime.now())
+                                .build())), List.class);
     }
 }
